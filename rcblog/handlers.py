1e766ce2067e370e34783280e6f63e969d943f34
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2015-10-18 00:28:25 +0300

    Add basic auth

--- rcblog/handlers.py
+++ rcblog/handlers.py
@@ -1,8 +1,35 @@
-from flask import Flask, render_template, request, redirect, url_for
+from functools import wraps
+
+from flask import Flask, render_template, request, redirect, url_for, Response
 from rcblog import utils
 
-
 app = Flask(__name__)
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'secret'
+
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+
+    return decorated
 
 
 @app.route('/')
@@ -16,6 +43,7 @@ def post(post_id):
 
 
 @app.route('/posts/add')
+@requires_auth
 def add_post():
     return render_template('add_post.html')
 
